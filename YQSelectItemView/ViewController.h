9e1629b97da409560aac16af0d6a43326a71c37a
Wang <wyqpadding@gmail.com>
2017-05-23 11:49:57 +0800

    Initial Commit

---
+++ YQSelectItemView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQSelectItemView
+//
+//  Created by Wang on 2017/5/23.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQSelectItemView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YQSelectItemView
+//
+//  Created by Wang on 2017/5/23.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
